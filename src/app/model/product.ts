e479ec1910dab94f2d0e616883e35b22a3a53d7d
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-04-24 10:43:16 +0800

    1

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -6,4 +6,6 @@ export class Product {
     public image:string
 ){}
 
+
+
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   constructor() { }
-  name = 'Nike球鞋';
-  price = 2000;
-  file = 'assets/nike.jpg';
-  onsale = false;
-  quality:number;
+  public name = 'Nike球鞋';
+  public price = 2000;
+  public file = 'assets/nike.jpg';
+  public onsale = false;
+  public quality:number;
+  public classes;
+  public numbers:Array<number>;
+
   toggleIsOnSale(){
     this.onsale = !this.onsale;
   }
@@ -21,9 +24,13 @@ export class ProductComponent implements OnInit {
   reduceQuality(){
     this.quality -= 1;
   }
-
   ngOnInit() {
     this.quality = 1;
+    this.numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    this.classes = {
+      'isOnSale':this.onsale,
+      'notOnSale':!this.onsale
+    };
   }
 
 }
