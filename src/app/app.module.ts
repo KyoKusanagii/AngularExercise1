33be70bd4d0f940197fb41a77b2acce27bd6ecc3
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-04-26 10:25:33 +0800

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CreateProductComponent } from './create-product/create-product.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from "../model/product";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-product',
@@ -9,15 +10,39 @@ import {Product} from "../model/product";
 export class CreateProductComponent implements OnInit {
 
   public product:Product;
+  public productForm:FormGroup;
 
-  constructor() {
-    this.product = new Product('Mac',70000,'我家');
+
+  constructor(private formbuilder:FormBuilder) {
+    // this.product = new Product('Mac',70000,'我家');
+    this.productForm = this.formbuilder.group({
+      name:[null,Validators.required],
+      price:[null,[Validators.required,Validators.min(1)]],
+      image:[null,Validators.required]
+    });
   }
   createProduct(productForm){
     console.log('創造一個產品表單',productForm);
     console.log('創造一個產品',this.product);
   }
+
+  get name() {
+    return this.productForm.get('name');
+  }
+
+  get price() {
+    return this.productForm.get('price');
+  }
+
+  get image() {
+    return this.productForm.get('image');
+  }
+
+  onSubmit() {
+    console.log('提交的表單為:', this.productForm.value);
+  }
   ngOnInit() {
+
   }
 
 }
