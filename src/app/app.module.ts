654b136517a74fe4a2d423b24b2592e3ecff4d04
Max.Wu_吳承恩 <max.wu@testritegroup.com>
2019-07-03 11:34:52 +0800

    可以撈取automail顯示在前端

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CreateProductComponent } from './create-product/create-product.component';
+import {AppComponent} from './app.component';
+import {ProductComponent} from './product/product.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {CreateProductComponent} from './create-product/create-product.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Report001Component} from './automail/report001/report001.component';
+import {HttpClientModule} from '@angular/common/http';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductListComponent,
-    CreateProductComponent
+    CreateProductComponent,
+    Report001Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/automail/common/system-constant.ts
@@ -0,0 +1,7 @@
+// rest api
+export const REST_SERVER_URL = 'http://localhost:8890/autoMailJob/autoMailRest';
+export const HTTP_OPTION = {
+  headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+};
+export const FORMATE_DATE = 'YYYY/MM/DD';
+export const LOCALE = 'en-US';
